aa641e6dcd53c1c4a4ca425c387335b25ebc35ef
Khandoker Tajuddin <khtajuddin97@gmail.com>
2022-09-30 20:13:51 +0600

    update working and it done my mysqli

--- update.php
+++ update.php
@@ -1,6 +1,15 @@
 <?php
     include 'connect.php';
     $id=$_GET['updateid'];
+    $sql="Select * from `crud` where id=$id";
+    $result=mysqli_query($conn,$sql);
+    $row=mysqli_fetch_assoc($result);
+    $name=$row['name'];
+    $email=$row['email'];
+    $phone=$row['phone'];
+    $password=$row['password'];
+
+
     if(isset($_POST['submit'])){
         $name=$_POST['name'];
         $email=$_POST['email'];
@@ -10,8 +19,8 @@
         $sql="update `crud` set id=$id,name='$name',email='$email',phone='$phone',password='$password' where id=$id";
         $result=mysqli_query($conn,$sql);
         if($result){
-            echo "Data updated successfully";
-            // header('location:display.php');
+            // echo "Data updated successfully";
+            header('location:display.php');
         }else{
             die(mysqli_error($conn));
         }
@@ -38,24 +47,24 @@
 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Name</label>
-    <input type="text" class="form-control" placeholder="Enter your Name" name="name" autocomplete="off">
+    <input type="text" class="form-control" placeholder="Enter your Name" name="name" autocomplete="off" value=<?php echo $name;?>>
   </div>
 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" placeholder="Enter your Email address" name="email" autocomplete="off">
+    <input type="email" class="form-control" placeholder="Enter your Email address" name="email" autocomplete="off" value=<?php echo $email;?>>
     <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
   </div>
 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Phone</label>
-    <input type="number" class="form-control" placeholder="Enter your mobile number" name = "phone" autocomplete="off">
+    <input type="number" class="form-control" placeholder="Enter your mobile number" name = "phone" autocomplete="off" value=<?php echo $phone;?>>
     
   </div>
 
   <div class="mb-3">
     <label for="exampleInputEmail1" class="form-label">Password</label>
-    <input type="password" class="form-control" placeholder="Enter your password" name = "password" autocomplete="off">
+    <input type="password" class="form-control" placeholder="Enter your password" name = "password" autocomplete="off" value=<?php echo $password;?>>
   </div>
  
   <button type="submit" class="btn btn-primary" name="submit">Update</button>
